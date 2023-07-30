61383b0d05144b85aedd6b828290b7e34e8396d2
Umoh Samuel Godwin <youngking074@gmail.com>
2023-07-30 07:50:47 +0100

    Update console.js

--- console.js
+++ console.js
@@ -4,3 +4,9 @@ console.log("Thanks for all being here") //second comment
 
 const a = 'this is an invitation to the team'
 console.log(a)
+
+//DOnt mind me
+
+const function Retro {
+  return null
+}
